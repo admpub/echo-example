14b34b312ede88f468fd6556ddb6c6e7f701459d
swh <swhbox@foxmail.com>
2016-02-17 15:06:56 +0800

    update

--- _v2/main.go
+++ _v2/main.go
@@ -10,10 +10,12 @@ import (
 )
 
 func main() {
-	engine := "fasthttp"
+	engine := "default"
 
 	e := echo.New()
 	e.Use(mw.Log())
+	e.Use(mw.Recover())
+	//e.Use(mw.Gzip())
 	e.Get("/", echo.HandlerFunc(func(c echo.Context) error {
 		return c.String(200, "Hello, World!\n"+fmt.Sprintf("%+v", c.Request().Form().All()))
 	}))
