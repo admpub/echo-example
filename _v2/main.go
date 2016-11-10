1a1b94416925cd2d82b02c93de7d5ff2115fa36b
swh <swhbox@foxmail.com>
2016-11-10 14:47:21 +0800

    update

--- _v2/main.go
+++ _v2/main.go
@@ -21,6 +21,7 @@ func main() {
 
 	e := echo.New()
 	e.SetDebug(true)
+
 	// ==========================
 	// 添加中间件
 	// ==========================
@@ -39,26 +40,43 @@ func main() {
 	e.Use(mw.Recover())
 	e.Use(mw.Gzip())
 
+	// 静态文件服务 (URL: http://localhost:4444/static/)
+	e.Use(mw.Static(&mw.StaticOptions{
+		Root:"static", //存放静态文件的物理路径
+		Path:"/static/", //网址访问静态文件的路径
+		Browse:true, //是否在首页显示文件列表
+	}))
+
 	// ==========================
 	// 设置路由
 	// ==========================
+
+	// 首页 (URL: http://localhost:4444/)
 	e.Get("/", func(c echo.Context) error {
 		return c.String(200, "Hello, World!\n"+fmt.Sprintf("%+v", c.Request().Form().All()))
 	})
 	e.Post("/", func(c echo.Context) error {
 		return c.String(200, "Hello, World!\n"+fmt.Sprintf("%+v", c.Request().Form().All()))
 	})
+
+	// Bind (URL: http://localhost:4444/bind)
 	e.Post("/bind", func(c echo.Context) error {
 		m := &FormData{}
 		c.Bind(m)
 		return c.String(200, "Bind data:\n"+fmt.Sprintf("%+v", m))
 	})
+
+	// v2 (URL: http://localhost:4444/v2)
 	e.Get("/v2", func(c echo.Context) error {
 		return c.String(200, "Echo v2")
 	})
+
+	// ping (URL: http://localhost:4444/ping)
 	e.Get("/ping", func(c echo.Context) error {
 		return c.String(200, "pong")
 	})
+	
+	// Stdlib handler (URL: http://localhost:4444/std)
 	e.Get("/std", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(`standard net/http handleFunc`))
 		w.WriteHeader(200)
@@ -67,13 +85,19 @@ func main() {
 	// ==========================
 	// 创建子路由
 	// ==========================
+
+	// GET (URL: http://localhost:4444/admin)
 	g := e.Group("/admin")
 	g.Get("", func(c echo.Context) error {
 		return c.String(200, "Hello, Group!\n"+fmt.Sprintf("%+v", c.Request().Form().All()))
 	})
+
+	// POST (URL: http://localhost:4444/admin)
 	g.Post("", func(c echo.Context) error {
 		return c.String(200, "Hello, Group!\n"+fmt.Sprintf("%+v", c.Request().Form().All()))
 	})
+
+	// (URL: http://localhost:4444/admin/ping)
 	g.Get("/ping", func(c echo.Context) error {
 		return c.String(200, "pong -- Group")
 	})
